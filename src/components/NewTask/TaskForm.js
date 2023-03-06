fc60a57466275f8472321000a8fc28b0dc748adb
Harish1651999 <hk1651999@gmail.com>
2023-03-06 09:30:40 +0530

    add css files.

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./TaskForm.module.css";
 
 const TaskForm = () => {
   return <></>;
--- src/components/Tasks/TaskItem.js
+++ src/components/Tasks/TaskItem.js
@@ -1,4 +1,5 @@
 import React from "react";
+import classes from "./TaskItem.module.css";
 
 const TaskItem = () => {
   return <></>;
