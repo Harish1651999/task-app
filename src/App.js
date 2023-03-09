7c2be9d4eda18bac73b70bae63d9c10df856296c
Harish1651999 <hk1651999@gmail.com>
2023-03-09 09:30:57 +0530

    add tasks component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NewTask from "./components/NewTask/NewTask";
+import Tasks from "./components/Tasks/Tasks";
 
 function App() {
   return (
     <>
       <NewTask />
+      <Tasks />
     </>
   );
 }
--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,8 +1,15 @@
 import React from "react";
+import Section from "../UI/Section";
 import classes from "./Tasks.module.css";
 
 const Tasks = () => {
-  return <></>;
+  return (
+    <Section>
+      <div className={classes.container}>
+        <h2>No tasks found. Start adding some!</h2>
+      </div>
+    </Section>
+  );
 };
 
 export default Tasks;
