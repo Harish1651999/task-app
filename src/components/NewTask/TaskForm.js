5368ebb11672898248f6ff8e21b1629905971206
Harish1651999 <hk1651999@gmail.com>
2023-03-10 09:27:18 +0530

    add form submission value in useRef hook.

--- src/components/NewTask/TaskForm.js
+++ src/components/NewTask/TaskForm.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./TaskForm.module.css";
 
 const TaskForm = () => {
+  const taskInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredValue = taskInputRef.current.value;
+
+    console.log(enteredValue);
+  };
+
   return (
-    <form className={classes.form}>
-      <input type="text" />
+    <form className={classes.form} onSubmit={submitHandler}>
+      <input type="text" ref={taskInputRef} />
       <button>Add Task</button>
     </form>
   );
